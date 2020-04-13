890f11191fb08e6da3dc4861928572131f109e34
김준영 <kimjy392@gmail.com>
2020-04-13 17:29:30 +0900

    Add | Header

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <Header></Header>
     <v-content>
        <router-view></router-view>
     </v-content>
@@ -7,13 +8,13 @@
 </template>
 
 <script>
-
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'App',
 
   components: {
-
+    Header,
   },
 
   data: () => ({
