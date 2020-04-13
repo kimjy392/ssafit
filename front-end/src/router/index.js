c202402b8cf65faba705f3ae10c7bf16fd239931
banggeut01 <genie121110@gmail.com>
2020-04-13 17:00:22 +0900

    Frontend | Update router - 로그인 path 추가

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '@/components/Login'
 
 Vue.use(VueRouter)
-
   const routes = [
   {
     path: '/',
@@ -17,6 +17,11 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
@@ -27,3 +32,4 @@ const router = new VueRouter({
 })
 
 export default router
+
