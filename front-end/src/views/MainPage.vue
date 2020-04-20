a1ed600330e323e71f333d6726781e8dbf0fb97c
banggeut01 <genie121110@gmail.com>
2020-04-20 17:10:31 +0900

    S02P23B104-28 | front | Update MainPage - Add searchbar

--- front-end/src/views/MainPage.vue
+++ front-end/src/views/MainPage.vue
@@ -1,17 +1,30 @@
 <template>
   <div>
-  <Header></Header>
-  main
+    <Header></Header>
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="6">
+            <v-text-field 
+            filled 
+            prepend-inner-icon="search"
+            rounded
+            dense
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header.vue'
+  import Header from '@/components/Header.vue'
 
-export default {
-  name: 'Main',
-  components: {
-    Header
+  export default {
+    name: 'Main',
+    components: {
+      Header
+    }
   }
-}
 </script>
