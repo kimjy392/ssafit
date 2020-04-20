c50bb08b5f6665e93f8a580275b0593f7f77b84f
Kimheejoo <delight_joo@naver.com>
2020-04-20 15:41:41 +0900

    200420 | server mp4 | S02P23B104-40

---
+++ back-end/posenet/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <my-video :sources="video.sources" :options="video.options"></my-video>
+  </div>
+</template>
+
+<script>
+import myVideo from 'vue-video'
+export default {
+    name: 'App',
+    data() {
+      return {
+        path: '',
+         video: {
+                sources: [{
+                    src: 'http://i02b104.p.ssafy.io/video/dance.mp4',
+                    type: 'video/mp4'
+                }],
+                options: {
+                    volume: 0.6
+                }
+         }
+      }
+    },
+    components: {
+        myVideo
+    }
+  }
+  
+</script>
+
+<style>
+
+</style>
