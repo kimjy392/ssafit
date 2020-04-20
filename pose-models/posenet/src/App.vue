319bd334ffe5fc3140807dd4f1233d322f420a74
yeomkyeorae <duarufp06@gmail.com>
2020-04-20 10:50:27 +0900

    200420 | enroll Trainer comp to App

--- pose-models/posenet/src/App.vue
+++ pose-models/posenet/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <Trainer />
   </div>
 </template>
 
 <script>
+import Trainer from './components/Trainer.vue'
 
 export default {
   name: 'App',
   components: {
-
+    Trainer
   }
 }
 </script>
