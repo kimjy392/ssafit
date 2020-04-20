aa98cb05051aea478bdc9bcf2c414345d14cdfb8
yeomkyeorae <duarufp06@gmail.com>
2020-04-20 09:40:07 +0900

    200420 | add posenet API

---
+++ pose-models/posenet/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
