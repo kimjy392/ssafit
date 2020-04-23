1e21df1fbb382e010e455cc591b6badf46e515db
yeomkyeorae <duarufp06@gmail.com>
2020-04-23 10:01:49 +0900

    200423 | add sketch.js

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import VueSession from 'vue-session'
+
 Vue.use(VueSession)
 Vue.config.productionTip = false
 
