b6200dae73d79f435e5157974554ca6d8579d668
yeomkyeorae <duarufp06@gmail.com>
2020-04-25 20:07:16 +0900

    200425 | update sketch.js

--- front-end/src/views/DetailPage.vue
+++ front-end/src/views/DetailPage.vue
@@ -1,12 +1,10 @@
 <template>
 	<div>
 		<Header></Header>
-		<!-- <h1>PoseNet demo with Ml5.js</h1> -->
-		<!-- <p id="status">Loading Posenet Model...</p> -->
-		<h1>코사인 유사도: {{ cosineSimilarity }}</h1>
-		<p id="countdown"></p>
+		<h1>Score: {{ score }}</h1>
 		<div id="videoContainer"></div>
 		<div class='sketch' id="sketch"></div>
+		<h1> {{ cosineSimilarity }} </h1>
 	</div>
 </template>
 
@@ -15,7 +13,9 @@
 	import axios from 'axios'
 	import sketch from './sktech';
 	import p5 from 'p5';
-	import { poseSimilarity } from 'posenet-similarity';
+	import {
+		poseSimilarity
+	} from 'posenet-similarity';
 
 	export default {
 		name: "Detail",
@@ -24,10 +24,13 @@
 		},
 		data() {
 			return {
-				// poses: poses
 				cam_poses: 0,
 				video_poses: 0,
 				cosineSimilarity: 0,
+				excellentThresh: 100,
+				greatThresh: 100,
+				goodThresh: 100,
+				score: 'Hmm...',
 			};
 		},
 		methods: {
@@ -37,10 +40,21 @@
 						this.cam_poses = window.cam_poses
 						this.video_poses = window.poses
 						try {
-							this.cosineSimilarity = poseSimilarity(this.cam_poses[0].pose, this.video_poses[0].pose, { strategy: 'cosineSimilarity'});
-						}
-						catch(err) {
-							console.log(11)
+							this.cosineSimilarity = poseSimilarity(this.cam_poses[0].pose, this.video_poses[0].pose, {
+								strategy: 'cosineSimilarity'
+							});
+							window.cosineSimilarity = this.cosineSimilarity
+							if (this.cosineSimilarity >= this.excellentThresh * 0.01) {
+								this.score = 'Excellent'
+							} else if (this.cosineSimilarity >= this.greatThresh * 0.01) {
+								this.score = 'Great'
+							} else if (this.cosineSimilarity >= this.goodThresh * 0.01) {
+								this.score = 'Good'
+							} else {
+								this.score = 'Hmm...'
+							}
+						} catch (err) {
+							this.score = 'Hmm...'
 						}
 					}, 1000);
 				});
@@ -48,40 +62,34 @@
 			getVideo() {
 				axios.get('http://i02b104.p.ssafy.io:8197/ssafyvue/api/stretching/detail/' + this.$route.params.id)
 					.then((res) => {
-						window.videoURL = 'https://i02b104.p.ssafy.io/video/' + res.data.file
+						this.excellentThresh = res.data.excellent;
+						this.greatThresh = res.data.great;
+						this.goodThresh = res.data.good;
+						window.excellentThresh = this.excellentThresh;
+						window.firstStop = res.data.first_stop;
+						window.secondStop = res.data.second_stop;
+						window.next = res.data.next;
+						window.videoURL = 'https://i02b104.p.ssafy.io/video/' + res.data.file;
 						this.myp5 = new p5(sketch, document.getElementById('sketch'))
 					})
 					.catch((err) => {
 						console.log(err)
 					})
 			},
-			timer() {
-				var timeleft = 9
-				var downloadTimer = setInterval(function () {
-					if (timeleft <= 0) {
-						clearInterval(downloadTimer);
-						document.getElementById("countdown").innerHTML = "Started!!!";
-					} else {
-						document.getElementById("countdown").innerHTML = 'Coaching will start within ' + timeleft +
-							" seconds remaining";
-					}
-					timeleft -= 1;
-				}, 1000);
-			}
 		},
 		mounted() {
-			this.timer();
+			if (localStorage.getItem('reloaded')) {
+				localStorage.removeItem('reloaded');
+			} else {
+				localStorage.setItem('reloaded', '1');
+				location.reload();
+			}
 			this.getVideo()
 			this.everySecondTrigger();
 		},
 	};
 </script>
-
 <style>
-	/* #videoContainer2 {
-  width: 640px;
-  display: inline-block;
-} */
 	canvas {
 		width: 100px;
 		height: 100px;
