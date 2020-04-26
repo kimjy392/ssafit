f3e42771424d60695bcb96fe091b3fb4d556053b
banggeut01 <genie121110@gmail.com>
2020-04-26 17:41:25 +0900

    S02P23B104-50 | Frontend | Header - Go 버튼 효과

--- front-end/src/components/Header.vue
+++ front-end/src/components/Header.vue
@@ -1,10 +1,5 @@
 <template>
-  <v-app-bar 
-    class="px-7" 
-    app 
-    flat 
-    color="#556AE9"
-    height="100">
+  <v-app-bar class="px-7" app flat color="#556AE9" height="100">
     <v-toolbar-title>
       <router-link to="/">
         <v-img contain width="200px" height="55px" src="@/assets/logo_test.png"></v-img>
@@ -13,37 +8,27 @@
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
       <div class="my-auto mx-2">
-        <v-btn 
-        text
-        color="white"
-        >
-          GO
+        <v-btn large text color="white" v-on:mouseover="mouseover" v-on:mouseleave="mouseleave">
+          <div v-if="is_hover">
+            <img height="44px" src="@/assets/fire_btn.gif" alt="animated_fire">
+          </div>
+          <div v-else>
+            GO
+          </div>
         </v-btn>
       </div>
       <div v-if="isLogin" class="my-auto mx-2">
-        <v-btn 
-        text
-        color="white"
-        >
+        <v-btn large text color="white">
           MY PAGE
         </v-btn>
       </div>
       <div v-if="!isLogin" class="my-auto mx-2">
-        <v-btn 
-          large
-          color="#FF7033"
-          key="LOG IN" 
-          to="/login"
-          style="color: white;">
+        <v-btn large color="#FF7033" key="LOG IN" to="/login" style="color: white;">
           LOG IN
         </v-btn>
       </div>
       <div v-if="isLogin" class="my-auto mx-2">
-        <v-btn 
-          large
-          color="#FF7033"
-          key="LOG OUT" 
-          style="color: white;">
+        <v-btn large color="#FF7033" key="LOG OUT" style="color: white;">
           LOG OUT
         </v-btn>
       </div>
@@ -62,11 +47,19 @@
     },
     data() {
       return {
+        is_hover: false
       }
     },
     computed() {
       this.$store.dispatch('isLogin');
     },
-    
+    methods: {
+      mouseover: function () {
+        this.is_hover = true
+      },
+      mouseleave: function () {
+        this.is_hover = false
+      }
+    }
   }
 </script>
