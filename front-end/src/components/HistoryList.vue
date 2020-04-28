1b8ebf9b430f11977b92492f3ba59bafbb60629c
kimjunyoung <kimjy392@gmail.com>
2020-04-28 22:46:56 +0900

    frontend Update | UserinfoPage grid

---
+++ front-end/src/components/HistoryList.vue
@@ -0,0 +1,17 @@
+<template>
+    <History></History>
+</template>
+
+<script>
+import History from '@/components/History.vue'
+export default {
+    name: 'HistoryList',
+    components: {
+        History
+    }
+}
+</script>
+
+<style>
+
+</style>
